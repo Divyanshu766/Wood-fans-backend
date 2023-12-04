32459a2aa34a4e3041f444f9adb30db365a7b0aa
Divyanshu766 <lalitarawat90123@gmail.com>
2023-12-04 16:09:51 +0530

    Add filter,sorting and serching functionality

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -2,6 +2,35 @@ const express = require("express");
 const { ProductModel } = require("../models/ProductModel");
 const ProductRoute = express.Router();
 
+ProductRoute.get("/", async (req, res) => {
+  try {
+    const { sort, search, order, type } = req.query;
+    const query = {};
+    const options = {};
+
+    // Filter by Types or Category :-
+    if (type) {
+      query.type = type;
+    }
+
+    // Filter by search term in the title
+    if (search) {
+      query.title = new RegExp(search, "i");
+    }
+
+    //Sort by Price :-
+    if (sort == "price") {
+      options.sort = { price: order === "asc" ? 1 : -1 };
+    }
+
+    const data = await ProductModel.find(query, null, options);
+    console.log(data);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 ProductRoute.post("/add", async (req, res) => {
   try {
     const { image, price, title, type, url1, url2, url3 } = req.body;
